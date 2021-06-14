1e3e2eafd69b76e5ecfb661af3b58c483db3a4e1
LohZheungYik <62550487+LohZheungYik@users.noreply.github.com>
2021-06-14 11:11:07 +0800

    Fix user session bug

--- src/main/java/com/headcount/headcount/exam/ExamController.java
+++ src/main/java/com/headcount/headcount/exam/ExamController.java
@@ -1,9 +1,10 @@
 package com.headcount.headcount.exam;
 
-import com.headcount.headcount.exam.Exam;
+import com.headcount.headcount.admin.Admin;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
@@ -12,21 +13,22 @@ public class ExamController {
     @GetMapping({"/exam/index"})
     public String index(HttpServletRequest request){
 
-        Exam exam = (Exam)request.getSession().getAttribute("exam");
+        Admin admin = (Admin)request.getSession().getAttribute("user");
 
-        if(exam != null){
+        if(admin != null){
             return "exam/index";
         }else{
             return "redirect:/";
         }
+
     }
 
     @GetMapping({"/exam/create"})
     public String create(HttpServletRequest request){
 
-        Exam exam = (Exam)request.getSession().getAttribute("user");
+        Admin admin = (Admin)request.getSession().getAttribute("user");
 
-        if(exam != null){
+        if(admin != null){
             return "exam/create";
         }else{
             return "redirect:/";
@@ -36,9 +38,9 @@ public class ExamController {
     @GetMapping({"/exam/update/{id}"})
     public String update(@PathVariable("id")int id, Map<String, Object> model, HttpServletRequest request){
 
-        Exam exam = (Exam)request.getSession().getAttribute("user");
+        Admin admin = (Admin)request.getSession().getAttribute("user");
 
-        if(exam != null){
+        if(admin != null){
             model.put("message", id);
             return "exam/update";
         }else{
